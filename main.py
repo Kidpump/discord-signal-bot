ad8dbde8757cfb7edfc601e8f6195b62f56ce1ba
Kidpump <kidpump@web.de>
2026-06-03 16:28:06 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+import discord
+import requests
+import os
+
+DISCORD_TOKEN = os.environ["DISCORD_TOKEN"]
+CHANNEL_ID = int(os.environ["CHANNEL_ID"])
+TELEGRAM_BOT_TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
+TELEGRAM_CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
+
+client = discord.Client(intents=discord.Intents.all())
+
+@client.event
+async def on_ready():
+    print(f"Bot läuft!")
+
+@client.event
+async def on_message(message):
+    if message.channel.id == CHANNEL_ID:
+        text = f"🚨 Neues Trading Signal:\n\n{message.content}"
+        requests.post(
+            f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage",
+            data={"chat_id": TELEGRAM_CHAT_ID, "text": text}
+        )
+
+client.run(DISCORD_TOKEN)
